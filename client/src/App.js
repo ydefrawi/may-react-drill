f88dbf95a2c4be23b80f6c7dd3e4fda9ac6f9759
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2022-05-18 08:57:58 -0500

    proxy/port issues fixed, server talking to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,33 @@
+import {React, useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [data, setData]=useState(); 
+  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // fetch data from the api
+      const res = await fetch("/api");
+      // convert the data to json
+      const jsonRes = await res.json();
+      // set state with the result
+      setData(jsonRes.message);
+    }
+    fetchData()
+      .catch(console.error);;
+   
+  })
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {!data ? "Loading..." : data}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );

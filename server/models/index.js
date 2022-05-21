d2b25376af8cd0b0cb1c56ad5709924f0d23b00c
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2022-05-21 12:38:51 -0500

    wrote model relationships in models/index.js

--- server/models/index.js
+++ server/models/index.js
@@ -3,6 +3,39 @@ import Instructors from './Instructors';
 import Students from './Students';
 import Signups from './Signups'
 
-Student.hasMany(Signups,{
-    foreignkey:'student_id'
+//Students/Signups relationship
+Students.hasMany(Signups, {
+    foreignKey: 'student_id'
+})
+
+Signups.belongsTo(Students, {
+    foreignKey: 'student_id'
+})
+
+//Students/Signups relationship
+Instructors.hasMany(Signups, {
+    foreignKey: 'instructor_id'
+})
+
+Signups.belongsTo(Instructors, {
+    foreignKey: 'instructor_id'
+})
+
+//Classes/Signups relationship
+Classes.hasMany(Signups, {
+    foreignKey: 'class_id'
+})
+
+Signups.belongsTo(Classes, {
+    foreignKey: 'class_id'
+})
+
+//Instructors/Classes relationship
+//!May use Signups as junction table instead 
+Instructors.hasMany(Classes, {
+    foreignkey: 'instructor_id'
+})
+
+Classes.belongsTo(Instructors, {
+    foreignKey: 'instructor_id'
 })

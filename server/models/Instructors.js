dd0c2c4ae258e476ce4b99b2daaa2290afbcb75a
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2022-05-21 11:54:09 -0500

    wrote Instructors model

---
+++ server/models/Instructors.js
@@ -0,0 +1,45 @@
+import Sequelize from 'sequelize';
+const { Model, DataTypes } = Sequelize;
+import { sequelize } from '../config/connection.js';
+
+class Instructors extends Model { }
+
+Instructors.init(
+    {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type:DataTypes.STRING,
+        validate:{
+            isEmail:true,
+        }
+    },
+    password: {
+        type:DataTypes.STRING
+    }
+
+},
+{
+    sequelize,
+    timestamps:false,
+    modelName:'Instructors'
+}
+);
+
+export default Instructors;
